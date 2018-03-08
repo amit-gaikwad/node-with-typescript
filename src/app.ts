50ba58936f387694e90325c631493f394ee09089
Amit Gaikwad <amit.gaikwad@weboniselab.com>
2018-03-08 12:10:50 +0530

    add initial config

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+const express = require('express');
+
+class App {
+  public express:any;
+
+  constructor () {
+    this.express = express()
+    this.mountRoutes()
+  }
+
+  private mountRoutes (): void {
+    const router = express.Router()
+    router.get('/', (req: any, res : any) => {
+      res.json({
+        message: 'Hello World!'
+      })
+    })
+    this.express.use('/', router)
+  }
+}
+
+export default new App().express;        
